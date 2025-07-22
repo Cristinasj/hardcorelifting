3395ae61196938807e1ae55b85de5334243424ab
veroAF5 <veronicasanchez.af5@gmail.com>
2025-07-22 18:09:20 +0200

    first commit

---
+++ backend/hardcorelifting/src/main/java/com/example/hardcorelifting/HardcoreliftingApplication.java
@@ -0,0 +1,13 @@
+package com.example.hardcorelifting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardcoreliftingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardcoreliftingApplication.class, args);
+	}
+
+}
---
+++ backend/hardcorelifting/src/test/java/com/example/hardcorelifting/HardcoreliftingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hardcorelifting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardcoreliftingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
