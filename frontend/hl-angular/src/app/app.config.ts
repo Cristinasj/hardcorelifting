6e3ee9561788c93309b41a9a46c24b26fb00b56c
inowen <inoah@correo.ugr.es>
2025-08-04 10:14:33 +0200

    Cleanup tests

--- frontend/hl-angular/src/app/app.config.ts
+++ frontend/hl-angular/src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth-interceptor';
 
 import { routes } from './app.routes';
 
@@ -7,6 +10,10 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    // Auth interceptor
+    provideHttpClient(withInterceptors([
+      (req, next) => { return authInterceptor(req, next); }
+    ])),
   ]
 };
