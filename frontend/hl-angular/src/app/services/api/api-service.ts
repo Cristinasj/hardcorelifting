f0ba216089f9ff915de996cf10b2c376780dfed8
inowen <inoah@correo.ugr.es>
2025-08-11 18:14:37 +0200

    Refactor: Config file for backend url

--- frontend/hl-angular/src/app/services/api/api-service.ts
+++ frontend/hl-angular/src/app/services/api/api-service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 /*
 This service gets data from the backend API 
@@ -11,25 +12,24 @@ and posts stuff to it (exercises, lifts, ...)
   providedIn: 'root'
 })
 export class ApiService {
+  private readonly BASE_URL = environment.apiBaseUrl;
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   async getExercises() {
-    const response: any = await firstValueFrom(this.http.get('http://localhost:3001/api/exercises'));
+    const response: any = await firstValueFrom(this.http.get(`${this.BASE_URL}/api/exercises`));
     return response.exercises || [];
   }
 
   async getUserStats() {
-    const response: any = await firstValueFrom(this.http.get('http://localhost:3001/api/user-info'));
+    const response: any = await firstValueFrom(this.http.get(`${this.BASE_URL}/api/user-info`));
     return response;
   }
 
-  async getExerciseHistory(exercise : string) {
-    const response: any = await firstValueFrom(this.http.get(`http://localhost:3001/api/exercises/${exercise}/history`));
+  async getExerciseHistory(exercise: string) {
+    const response: any = await firstValueFrom(this.http.get(`${this.BASE_URL}/api/exercises/${exercise}/history`));
     return response;
   }
 
 
-  
 }
